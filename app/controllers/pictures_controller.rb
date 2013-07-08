2b92f5aed87f2f644cbe0d42c1ad28ed65c40c32
Ian Gerald King <ian@iangeraldking.com>
2013-07-08 13:21:04 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,7 @@ class PicturesController < ApplicationController
 	end
 
 	def show
-	 	@pictures = Picture.find(params[:id])
+	 	@picture = Picture.find(params[:id])
 	end
 
 	def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,37 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 Picture.create!(
-		:title 	=> "The old church on the coast of the White sea",
-		:artist 	=> "Sergey Ershov",
-		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	)
+    :title  => "The old church on the coast of White sea",
+    :artist => "Sergey Ershov",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+  )
 
 Picture.create!(
-		:title	=> "Sea Power",
-		:artist	=> "Stephen Scullion",
-		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	)
+    :title  => "Sea Power",
+    :artist => "Stephen Scullion",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
+  )
 
 Picture.create!(
-		:title	=> "Into the Poppies",
-		:artist	=> "John Wilhelm", 
-		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	)
+    :title  => "Into the Poppies",
+    :artist => "John Wilhelm",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"  
+  )
+
+# Picture.create!(
+# 		:title 	=> "The old church on the coast of the White sea",
+# 		:artist 	=> "Sergey Ershov",
+# 		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+# 	)
+
+# Picture.create!(
+# 		:title	=> "Sea Power",
+# 		:artist	=> "Stephen Scullion",
+# 		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+# 	)
+
+# Picture.create!(
+# 		:title	=> "Into the Poppies",
+# 		:artist	=> "John Wilhelm", 
+# 		:url	=> "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+# 	)
